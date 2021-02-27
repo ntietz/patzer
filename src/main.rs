549e5d6306464b652d8f90b6b92f6b5e6dacbed6
Nicholas Tietz-Sokolsky <me@ntietz.com>
2021-02-27 13:11:53 -0500

    Remove the original code, start working on analysis mode

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,3 @@
-//use patzer::board::Board;
-use patzer::uci::{handle_message, parse_command, print_ready, print_uci_info, EngineMode, MsgOut};
-use std::io;
-
 fn main() {
-    //let board = Board::new();
-
-    let mut engine_mode = EngineMode::Init;
-
-    loop {
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Couldn't read user input.");
-
-        if let Some(msg_in) = parse_command(&input) {
-            let (msg_out, new_mode) = handle_message(msg_in, engine_mode);
-            engine_mode = new_mode;
-
-            match msg_out {
-                MsgOut::UciOk => print_uci_info(),
-                MsgOut::ReadyOk => print_ready(),
-            }
-        }
-        // silently ignore parsing failures for now
-        // TODO: better error handling
-        else {
-            println!("{}", input);
-        }
-    }
+    println!("Welcome to Patzer.");
 }
