0f01461c22d270ee199856986c9acfe4ce6a1c7e
Nicole Tietz-Sokolskaya <me@ntietz.com>
2022-09-22 16:18:33 -0400

    Add stub of GUI based on the eframe example

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # patzer
 a chess engine that hopefully plays better than *this* patzer does
+
+## Dependencies
+
+Since we're using [egui](https://crates.io/crates/egui), we have some dependencies to install before the GUI will run.
+
+On Fedora, run:
+
+```
+sudo dnf install clang clang-devel clang-tools-extra speech-dispatcher-devel libxkbcommon-devel pkg-config openssl-devel libxcb-devel cmake fontconfig-devel
+```
---
+++ src/bin/gui.rs
@@ -0,0 +1,43 @@
+use eframe::egui;
+
+
+pub fn main() {
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Patzer App",
+        options,
+        Box::new(|_cc| Box::new(PatzerApp::default())),
+    );
+}
+
+struct PatzerApp {
+    name: String,
+    age: u32,
+}
+
+impl Default for PatzerApp {
+    fn default() -> Self {
+        Self {
+            name: "Arthur".to_owned(),
+            age: 42,
+        }
+    }
+}
+
+impl eframe::App for PatzerApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Patzer Application");
+            ui.horizontal(|ui| {
+                ui.label("Your name: ");
+                ui.text_edit_singleline(&mut self.name);
+            });
+
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+            if ui.button("Click each year").clicked() {
+                self.age += 1;
+            }
+            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+        });
+    }
+}
