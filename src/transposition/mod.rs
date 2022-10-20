22a263f22d9c70ad367292ffd777fa4fbd489687
Nicholas Tietz-Sokolsky <me@ntietz.com>
2022-10-19 20:50:14 -0400

    Limit size of transposition table

--- src/transposition/mod.rs
+++ src/transposition/mod.rs
@@ -1,11 +1,10 @@
-use std::collections::HashMap;
-
 use chess::ChessMove;
 
 use crate::evaluation::Score;
 
 pub type Hash = u64;
 
+#[derive(Clone, Copy)]
 pub struct TableEntry {
     /// Hash (zobrist) of the current position
     pub hash: Hash,
@@ -20,6 +19,7 @@ pub struct TableEntry {
     pub following_move: Option<ChessMove>,
 }
 
+#[derive(Clone, Copy)]
 pub enum Evaluation {
     Exact(Score),
     Beta(Score),
@@ -27,18 +27,22 @@ pub enum Evaluation {
 }
 
 pub struct TranspositionTable {
-    transpositions: HashMap<Hash, TableEntry>,
+    //transpositions: HashMap<Hash, TableEntry>,
+    transpositions: Vec<Option<TableEntry>>,
+    size: usize,
     num_hits: usize,
     num_misses: usize,
 }
 
 impl TranspositionTable {
     pub fn new() -> Self {
-        let transpositions = HashMap::new();
+        let size = 1_000_000;
+        let transpositions = vec![None; size];
         let num_hits = 0;
         let num_misses = 0;
         TranspositionTable {
             transpositions,
+            size,
             num_hits,
             num_misses,
         }
@@ -61,22 +65,32 @@ impl TranspositionTable {
     }
 
     pub fn store(&mut self, hash: Hash, depth: u8, eval: Evaluation) {
-        let entry = TableEntry {
-            hash,
-            depth,
-            eval,
-            following_move: None,
-        };
-        self.transpositions.insert(hash, entry);
+        let position = hash as usize % self.size;
+
+        let entry = self.transpositions.get_mut(position).unwrap();
+        if entry.is_none() || entry.unwrap().depth < depth {
+            *entry = Some(TableEntry {
+                hash,
+                depth,
+                eval,
+                following_move: None,
+            });
+        }
     }
 
-    pub fn retrieve(&mut self, hash: Hash) -> Option<&TableEntry> {
-        let result = self.transpositions.get(&hash);
+    pub fn retrieve(&mut self, hash: Hash) -> Option<TableEntry> {
+        let position = hash as usize % self.size;
+
+        let result = self.transpositions.get(position).unwrap().filter(|p| {
+            p.hash == hash
+        });
+
         if result.is_some() {
             self.num_hits += 1;
         } else {
             self.num_misses += 1;
         }
+
         result
     }
 }
